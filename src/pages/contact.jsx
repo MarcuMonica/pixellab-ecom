ea5607be6c12dec73d10213735a4784417bf6005
MarcuMonica <monica_me5@yahoo.com>
2022-09-26 21:52:28 +0300

    first commit

---
+++ src/pages/contact.jsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+const ContactPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Contact Page</title>
+      </Head>
+
+      <main>Hello from Next</main>
+    </>
+  );
+};
+
+//este obligatoriu sa returnam un default export pentru fiecare pagina
+
+export default ContactPage;
---
+++ src/pages/index.jsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Homepage</title>
+      </Head>
+
+      <main className="bg-red-700 text-3xl font-bold underline">Homepage</main>
+    </>
+  );
+};
+
+export default Home;
